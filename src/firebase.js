f7ce4e0ef5072f1427465e2ded8274d464bd70c6
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-05-22 15:01:57 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBapKZDLdlzKpP5duBc53SU6pZ1aChBGkw",
-  authDomain: "proyectofinalreact-25034.firebaseapp.com",
-  projectId: "proyectofinalreact-25034",
-  storageBucket: "proyectofinalreact-25034.appspot.com",
-  messagingSenderId: "657920530734",
-  appId: "1:657920530734:web:057484e6bab03f3e5282bf",
-  measurementId: "G-21LW72Y543"
+  apiKey: "AIzaSyAolfH6K_fq_WH5pkmr-EpW8hQdKVGKo08",
+  authDomain: "tpfinal-react-c6c4d.firebaseapp.com",
+  projectId: "tpfinal-react-c6c4d",
+  storageBucket: "tpfinal-react-c6c4d.appspot.com",
+  messagingSenderId: "184961640325",
+  appId: "1:184961640325:web:e122b611251afe2aaec6ec",
+  measurementId: "G-8CP71FT4RY"
 };
 
 // Initialize Firebase
